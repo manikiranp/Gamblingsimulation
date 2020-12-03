d314778ddd62cf1ee18b5b429f0cd940f9c47b08
manikiranp <manikiranp3@gmail.com>
2020-12-03 10:01:55 +0530

    Welcome to Gambling simulation

---
+++ src/simulation/Simulation.java
@@ -0,0 +1,14 @@
+package simulation;
+
+public class Simulation {
+	 static int totalstake=100;
+	 int dailybet=1;
+
+	public static void main(String[] args) {
+		System.out.println("Gambling Simulation\nTotal Available bet: "+totalstake);
+		
+	
+
+	}
+
+}
