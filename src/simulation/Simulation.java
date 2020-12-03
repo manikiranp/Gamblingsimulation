c9493ce1dda379367e3f6588a90e840be00ea56c
manikiranp <manikiranp3@gmail.com>
2020-12-03 10:22:56 +0530

    Staking bet functionality added

--- src/simulation/Simulation.java
+++ src/simulation/Simulation.java
@@ -1,14 +1,30 @@
 package simulation;
 
+import java.util.Random;
+
 public class Simulation {
-	 static int totalstake=100;
-	 int dailybet=1;
+	 static final int totalstake=100;
+	 static int updatedstake=100;
+	 static final int dailybet=1;
 
 	public static void main(String[] args) {
 		System.out.println("Gambling Simulation\nTotal Available bet: "+totalstake);
-		
+		betting();
 	
 
+	}
+
+	private static void betting() {
+		Random random=new Random();
+		int outcome=random.nextInt(2);
+		if(outcome==1) {
+			System.out.println("Won the bet!");
+			updatedstake=updatedstake+1;
+		} else {
+			System.out.println("Lost the bet!");
+			updatedstake=updatedstake-1;
+		}
+		System.out.println("Remaining stake: "+updatedstake);
 	}
 
 }
