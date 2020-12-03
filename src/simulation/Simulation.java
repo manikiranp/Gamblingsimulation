595d126f78958b1b122e4a417b51f1cdf65ac32e
manikiranp <manikiranp3@gmail.com>
2020-12-03 19:52:42 +0530

    Added: Gambling for 20 days and checks for amount win or lost

--- src/simulation/Simulation.java
+++ src/simulation/Simulation.java
@@ -6,59 +6,67 @@ public class Simulation {
 	 static final int TOTALSTAKE=100;
 	 static int updatedstake=TOTALSTAKE;
 	 static final int DAILYBET=1;
-	static  int dailyresult=0;
+	 	static int totalwinlose=0;
+	
 
 	public static void main(String[] args) {
 		System.out.println("Gambling Simulation\nTotal Available bet: "+TOTALSTAKE);
-		//int result=Bettinglimit();
-		Betting20days();
-		
+		Bettingdays();
 	
-
 	}
 
-	private static void Betting20days() {
-		int day=1;
-		int totalwinlose=0;
-		while(day<=20) {
-			Bettinglimit();
-			day++;
-			totalwinlose+=dailyresult;
-		} 
+	private static void Bettingdays() {
 		
-		System.out.println("Total stake won or lose: "+totalwinlose);
+		int day=1;
+		while(day<=20) {
+			updatedstake=100;
+			System.out.println("Day---"+day);			
+			int result=Bettinglimit();
+			
+			totalwinlose=totalwinlose+result;
+			day++;
+		}
+		if(totalwinlose>0) { 
+		System.out.println("Total stake Won: "+totalwinlose);
+		if (totalwinlose<0) {
+			System.out.println("Total stake Lost: "+totalwinlose*-1);
+		}
+		} else {
+			System.out.println("No win nor lost: "+totalwinlose);
+		}
 		
 	}
 
 	private static int Bettinglimit() {
-		
+		int dailyresult=0;
 		while((updatedstake<(TOTALSTAKE*1.5)) && (updatedstake>(TOTALSTAKE*0.5))) {
 			betting();
-			
 		}
-		
-			if(updatedstake>0) {
+			if (updatedstake>TOTALSTAKE) {
+			dailyresult=updatedstake-TOTALSTAKE;
+			System.out.println("Result: won "+dailyresult );
+			}else {
 				dailyresult=updatedstake-TOTALSTAKE;
-			} else {
-				dailyresult=updatedstake-TOTALSTAKE;
+				System.out.println("Result: lost "+dailyresult*-1);
 			}
+		
 		return dailyresult;
 		
 	} 
 
-	private static int betting() {
+	private static void betting() {
 		
 		Random random=new Random();
 		int outcome=random.nextInt(2);
 		if(outcome==1) {
-			System.out.println("Won the bet!");
+			//System.out.println("Won the bet!");
 			updatedstake+=DAILYBET;
 		} else {
-			System.out.println("Lost the bet!");
-			updatedstake-=DAILYBET;
+			//System.out.println("Lost the bet!");
+			updatedstake=updatedstake-DAILYBET;
 		}
-		System.out.println("Remaining stake: "+updatedstake);
-		return outcome;
+		
+		
 	}
 
 }
